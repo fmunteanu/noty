0bcfb8187e890288c9be8bf732f8ad471747d9f4
Floren Munteanu <floren@axivo.com>
2023-01-22 10:46:23 -0500

    Initial commit

---
+++ theme/static/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    'use strict';
+    var typed = $('.typed');
+
+    $(function() {
+        typed.typed({
+            strings: [
+                'No Thank You'
+            ],
+            typeSpeed: 100,
+            loop: true,
+        });
+    });
+});
